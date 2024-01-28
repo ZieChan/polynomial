d2df9eb1d7f02a0bca87e6d8f3842a441f7816ed
ZieChan <1357287150@qq.com>
2024-01-27 21:45:21 -0500

    cs 506 exercise part b

--- polynomial.py
+++ polynomial.py
@@ -37,6 +37,29 @@ class Mul:
             return repr(self.p1) + " * ( " + repr(self.p2) + " )"
         return repr(self.p1) + " * " + repr(self.p2)
 
+class Div:
+    def __init__(self, p1, p2):
+        self.p1 = p1
+        self.p2 = p2
+
+    def __repr__(self):
+        p1_repr = f"( {repr(self.p1)} )" if isinstance(self.p1, (Add, Sub)) else repr(self.p1)
+        p2_repr = f"( {repr(self.p2)} )" if isinstance(self.p2, (Add, Sub)) else repr(self.p2)
+        return f"{p1_repr} / {p2_repr}"
+
+class Sub:
+    def __init__(self, p1, p2):
+        self.p1 = p1
+        self.p2 = p2
+
+    def __repr__(self):
+        p1_repr = f"( {repr(self.p1)} )" if isinstance(self.p1, Add) else repr(self.p1)
+        p2_repr = f"( {repr(self.p2)} )" if isinstance(self.p2, Add) else repr(self.p2)
+        return f"{p1_repr} - {p2_repr}"
+
 
 poly = Add(Add(Int(4), Int(3)), Add(X(), Mul(Int(1), Add(Mul(X(), X()), Int(1)))))
 print(poly)
+
+poly2 = Sub(Div(Int(4), Int(3)), Add(X(), Mul(Int(1), Add(Mul(X(), X()), Int(1)))))
+print(poly2)
